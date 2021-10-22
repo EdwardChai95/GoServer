1ff6ff07900ffe6fad84308ba7ee19e2bd14350c
EdwardChai <41204208+EdwardChai95@users.noreply.github.com>
2021-10-22 17:57:08 +0800

    Delete admin directory

--- admin/db/model/struct.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Config struct {
-	ConfigId int64  `xorm:"not null pk autoincr"`
-	Key      string `xorm:"unique"`
-	Value    string `xorm:"text"`
-}
--- admin/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nohup go run main.go &
