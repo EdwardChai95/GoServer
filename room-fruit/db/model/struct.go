e3855f9f5cfed0546b169128a427bd9dafee4376
EdwardChai <41204208+EdwardChai95@users.noreply.github.com>
2021-10-22 17:57:49 +0800

    Delete room-fruit directory

--- room-fruit/db/model/struct.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "time"
-
-type FruitWinningItem struct {
-	WinningItemsId int64 `xorm:"not null pk autoincr"`
-	WinningKey     int64
-	CreateAt       time.Time `xorm:"created index"`
-}
